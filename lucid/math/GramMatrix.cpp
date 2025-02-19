3e9e6dcf9a0695566f93361e8b0cabd2c39b7925
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-02-19 21:24:20 +0000

    fix: efficient kernel diagonal entries in gram_matrix

--- lucid/math/GramMatrix.cpp
+++ lucid/math/GramMatrix.cpp
@@ -19,13 +19,14 @@ GramMatrix::GramMatrix(const Kernel& kernel, Matrix initial_states, const double
       coefficients_{initial_states_.rows(), initial_states_.cols()} {
   if (!coefficients_.rows() || !coefficients_.cols()) LUCID_INVALID_ARGUMENT("states", "empty");
   // TODO(tend): should this be kernel(initial_states_.col(i), initial_states_.col(i))?
-  gram_matrix_.diagonal() = Vector::Constant(gram_matrix_.rows(), 1 + regularization_constant);
+  gram_matrix_.diagonal() = Vector::Constant(
+      gram_matrix_.rows(), kernel(initial_states_.row(0), initial_states_.row(0)) + regularization_constant);
   for (Index row_idx = 0; row_idx < gram_matrix_.rows(); ++row_idx) {
     for (Index col_idx = 0; col_idx < row_idx; ++col_idx) {
       gram_matrix_(row_idx, col_idx) = kernel(initial_states_.row(row_idx), initial_states_.row(col_idx));
     }
   }
-  LUCID_DEV_FMT("Computed gram matrix: \n[{}]", gram_matrix_);
+  LUCID_TRACE_FMT("Computed gram matrix: \n[{}]", gram_matrix_);
 }
 GramMatrix::GramMatrix(const Kernel& kernel, Matrix initial_states, const Matrix& transition_states,
                        const double regularization_constant)
@@ -38,7 +39,7 @@ void GramMatrix::compute_coefficients(const Matrix& transition_states) {
   if (transition_states.rows() != initial_states_.rows())
     LUCID_INVALID_ARGUMENT_EXPECTED("transition_states.rows()", transition_states.rows(), initial_states_.rows());
   coefficients_ = gram_matrix_.selfadjointView<Eigen::Lower>().ldlt().solve(transition_states);
-  LUCID_DEV_FMT("Computed coefficients: \n[{}]", coefficients_);
+  LUCID_TRACE_FMT("Computed coefficients: \n[{}]", coefficients_);
 }
 Vector GramMatrix::operator()(const Vector& state) const {
   if (coefficients_.size() == 0) LUCID_RUNTIME_ERROR("Coefficients have not been computed yet.");
@@ -46,7 +47,7 @@ Vector GramMatrix::operator()(const Vector& state) const {
     LUCID_INVALID_ARGUMENT_EXPECTED("state.size()", state.size(), initial_states_.rows());
   Vector kernel_state{Vector::NullaryExpr(
       gram_matrix_.rows(), [this, &state](const Index i) { return kernel_(initial_states_.row(i), state); })};
-  LUCID_DEV_FMT("Computed kernel state: \n[{}]", kernel_state);
+  LUCID_TRACE_FMT("Computed kernel state: \n[{}]", kernel_state);
   return kernel_state.transpose() * coefficients_;
 }
 Matrix GramMatrix::operator()(const Matrix& state) const {
@@ -56,7 +57,7 @@ Matrix GramMatrix::operator()(const Matrix& state) const {
   Matrix kernel_state{Matrix::NullaryExpr(
       gram_matrix_.rows(), state.rows(),
       [this, &state](const Index row, const Index col) { return kernel_(initial_states_.row(row), state.row(col)); })};
-  LUCID_DEV_FMT("Computed kernel state: \n[{}]", kernel_state);
+  LUCID_TRACE_FMT("Computed kernel state: \n[{}]", kernel_state);
   return kernel_state.transpose() * coefficients_;
 }
 const Matrix& GramMatrix::coefficients() const {
