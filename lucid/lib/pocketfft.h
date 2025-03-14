1434b32914578219da615d56ec88aa8fba591a28
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-03-14 17:49:29 +0000

    dep: add pocketfft lib

---
+++ lucid/lib/pocketfft.h
@@ -0,0 +1,14 @@
+/**
+ * @author c3054737
+ * @copyright 2025 lucid
+ * @licence BSD 3-Clause License
+ * @file
+ * pocketfft wrapper.
+ * This header includes the pocketfft library and provides a various helpers.
+ * Other files in the library should depend on this header instead of the pocketfft library directly.
+ * Instead of including <pocketfft_hdronly.h>, include "lucid/lib/pocketfft.h".
+ */
+
+#include <pocketfft_hdronly.h>
+
+namespace lucid {}
