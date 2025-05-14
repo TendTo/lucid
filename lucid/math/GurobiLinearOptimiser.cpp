3892dc7470cec74ae3ac4f50c40e3080a6d86094
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-05-14 17:07:53 +0100

    chore: add names to variables and constraints

--- lucid/math/GurobiLinearOptimiser.cpp
+++ lucid/math/GurobiLinearOptimiser.cpp
@@ -54,16 +54,28 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
   GRBVar& maxXX = vars[vars.size() - 2];
   GRBVar& minDelta = vars[vars.size() - 1];
 
+  // TODO(tend): we should make variable and constraint naming contingent on a debug flag
+  c.set(GRB_StringAttr_VarName, "c");
+  eta.set(GRB_StringAttr_VarName, "eta");
+  minX0.set(GRB_StringAttr_VarName, "minX0");
+  maxXU.set(GRB_StringAttr_VarName, "maxXU");
+  maxXX.set(GRB_StringAttr_VarName, "maxXX");
+  minDelta.set(GRB_StringAttr_VarName, "minDelta");
+
   // Variables related to the feature map
+  int idx = 0;
   for (GRBVar& var : vars.subspan(0, rkhs_dim)) {
     var.set(GRB_DoubleAttr_LB, -max_num);
     var.set(GRB_DoubleAttr_UB, max_num);
+    var.set(GRB_StringAttr_VarName, fmt::format("b[{}]", idx++));
   }
   c.set(GRB_DoubleAttr_LB, 0);
   c.set(GRB_DoubleAttr_UB, max_num);
   eta.set(GRB_DoubleAttr_LB, min_eta);
-  eta.set(GRB_DoubleAttr_UB, gamma_ - min_num);
-  for (GRBVar& var : std::array{minX0, maxXU, maxXX, minDelta}) {
+  eta.set(GRB_DoubleAttr_UB, gamma_ - min_num);  // To enforce a strict inequality, we add a small number to the ub
+  minDelta.set(GRB_DoubleAttr_LB, -max_num);
+  minDelta.set(GRB_DoubleAttr_UB, max_num);
+  for (GRBVar& var : std::array{minX0, maxXU, maxXX}) {
     var.set(GRB_DoubleAttr_LB, 0);
     var.set(GRB_DoubleAttr_UB, max_num);
   }
@@ -79,74 +91,82 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
   // 1) eta + c*T >= 0 by design
   // 2) eta + c*T <= gamma
   LUCID_DEBUG("Restricting safety probabilities to be positive");
-  model.addConstr(eta + T_ * c, GRB_LESS_EQUAL, gamma_);
+  model.addConstr(eta + T_ * c, GRB_LESS_EQUAL, gamma_).set(GRB_StringAttr_ConstrName, "eta+c*T<=gamma");
 
-  LUCID_DEBUG(
-      "Positive barrier\n"
+  // TODO(tend): since we are using row major order, there is no need to copy each row of the matrix.
+  //  Just add an assertion to make sure this is not changed at a later date, breaking the LP problem.
+  LUCID_DEBUG_FMT(
+      "Positive barrier - {} constraints\n"
       "for all x: [ B(x) >= hatxi ] AND [ B(x) <= maxXX ]\n"
-      "hatxi = (C - 1) / (C + 1) * maxXX");
+      "hatxi = (C - 1) / (C + 1) * maxXX",
+      phi_mat.rows() * 2);
   for (Index row = 0; row < phi_mat.rows(); ++row) {
     GRBLinExpr expr{};
     expr.addTerms(phi_mat.row(row).eval().data(), vars_.get(), static_cast<int>(phi_mat.cols()));
     expr += maxXX * maxXX_coeff;
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0);
-    expr -= maxXX * maxXX_coeff;
+    model.addConstr(expr, GRB_GREATER_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x)>=hatxi[{}]", row));
+    expr -= maxXX * maxXX_coeff;  // TODO(tend): use expr.remove(maxXX);
     expr += -maxXX;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0);
+    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x)<=maxXX[{}]", row));
   }
 
-  LUCID_DEBUG(
-      "Initial constraints\n"
+  LUCID_DEBUG_FMT(
+      "Initial constraints - {} constraints\n"
       "for all x_0: [ B(x_0) <= hateta ] AND [ B(x_0) >= minX0 ]\n"
-      "hateta = 2 / (C + 1) * eta + (C - 1) / (C + 1) * minX0");
+      "hateta = 2 / (C + 1) * eta + (C - 1) / (C + 1) * minX0",
+      f0_lattice.rows() * 2);
   for (Index row = 0; row < f0_lattice.rows(); ++row) {
     GRBLinExpr expr{};
     expr.addTerms(f0_lattice.row(row).eval().data(), vars_.get(), static_cast<int>(f0_lattice.cols()));
     expr += -fctr1 * eta - fctr2 * minX0;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0);
-    expr -= -fctr1 * eta - fctr2 * minX0;
+    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=hateta[{}]", row));
+    expr -= -fctr1 * eta - fctr2 * minX0;  // TODO(tend): use expr.remove(eta); expr.remove(minX0);
     expr += -minX0;
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0);
+    model.addConstr(expr, GRB_GREATER_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=minX0[{}]", row));
   }
 
-  LUCID_DEBUG(
-      "Unsafe constraints\n"
+  LUCID_DEBUG_FMT(
+      "Unsafe constraints - {} constraints\n"
       "for all x_u: [ B(x_u) >= hatgamma ] AND [ B(x_u) <= maxXU ]\n"
-      "hatgamma = 2 / (C + 1) * gamma + (C - 1) / (C + 1) * maxXU");
+      "hatgamma = 2 / (C + 1) * gamma + (C - 1) / (C + 1) * maxXU",
+      fu_lattice.rows() * 2);
   for (Index row = 0; row < fu_lattice.rows(); ++row) {
     GRBLinExpr expr{};
     expr.addTerms(fu_lattice.row(row).eval().data(), vars_.get(), static_cast<int>(fu_lattice.cols()));
     expr += -fctr2 * maxXU;
-    model.addConstr(expr, GRB_GREATER_EQUAL, unsafe_rhs);
-    expr -= -fctr2 * maxXU;
+    model.addConstr(expr, GRB_GREATER_EQUAL, unsafe_rhs)
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x_u)>=hatgamma[{}]", row));
+    expr -= -fctr2 * maxXU;  // TODO(tend): use expr.remove(maxXU);
     expr += -maxXU;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0);
+    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_u)<=maxXU[{}]", row));
   }
 
-  LUCID_DEBUG(
-      "Kushner constraints (verification case)\n"
+  LUCID_DEBUG_FMT(
+      "Kushner constraints (verification case) - {} constraints\n"
       "for all x: [ B_plus(x) - B(x) <= hatDelta ] AND [ B(x) >= minDelta ]\n"
-      "hatDelta = 2 / (C + 1) * (c - epsilon*Bnorm*kappa_x) + (C - 1) / (C + 1) * minDelta");
+      "hatDelta = 2 / (C + 1) * (c - epsilon*Bnorm*kappa_x) + (C - 1) / (C + 1) * minDelta",
+      phi_mat.rows() * 2);
   auto mult = w_mat - b_kappa_ * phi_mat;
-  for (Index row = 0; row < w_mat.rows(); ++row) {
+  for (Index row = 0; row < mult.rows(); ++row) {
     GRBLinExpr expr{};
     expr.addTerms(mult.row(row).eval().data(), vars_.get(), static_cast<int>(mult.cols()));
-    expr += -fctr1 * c - fctr2 * minDelta;
-    model.addConstr(expr, GRB_LESS_EQUAL, kushner_rhs);
-  }
-  for (Index row = 0; row < phi_mat.rows(); ++row) {
-    GRBLinExpr expr{-minDelta};
-    expr.addTerms(phi_mat.row(row).eval().data(), vars_.get(), static_cast<int>(phi_mat.cols()));
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0);
+    expr += -fctr1 * (c)-fctr2 * minDelta;  // TODO(tend): c − εB̄κ
+    model.addConstr(expr, GRB_LESS_EQUAL, kushner_rhs)
+        .set(GRB_StringAttr_ConstrName, fmt::format("B_plus(x)-B(x)<=hatDelta[{}]", row));
+    expr -= -fctr1 * (c)-fctr2 * minDelta;  // TODO(tend): use expr.remove(c); expr.remove(minDelta);
+    expr += -minDelta;
+    model.addConstr(expr, GRB_GREATER_EQUAL, 0)
+        .set(GRB_StringAttr_ConstrName, fmt::format("B_plus(x)-B(x)>=minDelta[{}]", row));
   }
 
-  // Objective function
-  model.setObjective(GRBLinExpr{T_ / gamma_ * c + 1 / gamma_ * eta});
+  // Objective function (η + cT)
+  model.setObjective(GRBLinExpr{(eta + c * T_) / gamma_});
 
   LUCID_INFO("Optimizing");
   model.optimize();
 
   if (model.get(GRB_IntAttr_SolCount) == 0) {
+    model.computeIIS();
     LUCID_INFO_FMT("No solution found, optimization status = {}", model.get(GRB_IntAttr_Status));
     cb(false, 0, Vector{}, 0, 0, 0);
     return false;
