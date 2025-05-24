0a2c887efa14d453ed2e97f53d84a2b565db86c0
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-05-24 16:12:03 +0100

    feat: add size to parameter values

--- lucid/model/ParameterValues.h
+++ lucid/model/ParameterValues.h
@@ -18,7 +18,7 @@ namespace lucid {
 
 /**
  * Collection of parameter values used in a configurable system or application.
- * It associates to the same parameter an arbitrarily large number of values.
+ * It associates with the same parameter an arbitrarily large number of values.
  * All values must have the same type, but the type can be different for each parameter.
  */
 class ParameterValues {
@@ -30,29 +30,35 @@ class ParameterValues {
    * @param parameter parameter to set
    * @param values all values assigned to the parameter
    */
-  ParameterValues(const Parameter parameter, std::vector<double> values) : parameter_(parameter), values_{values} {}
+  ParameterValues(const Parameter parameter, std::vector<double> values)
+      : parameter_(parameter), size_{values.size()}, values_{std::move(values)} {}
   /**
    * Construct a new ParameterValues object for the given `parameter` associated with the given `values`.
    * @param parameter parameter to set
    * @param values all values assigned to the parameter
    */
-  ParameterValues(const Parameter parameter, std::vector<int> values) : parameter_(parameter), values_{values} {}
+  ParameterValues(const Parameter parameter, std::vector<int> values)
+      : parameter_(parameter), size_{values.size()}, values_{std::move(values)} {}
   /**
    * Construct a new ParameterValues object for the given `parameter` associated with the given `values`.
    * @param parameter parameter to set
    * @param values all values assigned to the parameter
    */
-  ParameterValues(const Parameter parameter, std::vector<Vector> values) : parameter_(parameter), values_{values} {}
+  ParameterValues(const Parameter parameter, std::vector<Vector> values)
+      : parameter_(parameter), size_{values.size()}, values_{std::move(values)} {}
 
   template <IsAnyOf<int, double, Vector>... T>
     requires(sizeof...(T) > 0)
   explicit ParameterValues(const Parameter parameter, T... values)
       : parameter_(parameter),
+        size_{sizeof...(T)},
         values_{std::vector<std::tuple_element_t<0, std::tuple<T...>>>{
             std::forward<std::tuple_element_t<0, std::tuple<T...>>>(values)...}} {}
 
   /** @getter{parameter, parameter values} */
   [[nodiscard]] Parameter parameter() const { return parameter_; }
+  /** @getter{size, parameter values} */
+  [[nodiscard]] std::size_t size() const { return size_; }
   /** @getter{values, parameter values} */
   [[nodiscard]] const ParameterValuesType &values() const { return values_; }
   /**
@@ -67,9 +73,18 @@ class ParameterValues {
 
  private:
   Parameter parameter_;         ///< Parameter the values are assigned to
+  std::size_t size_;            ///< Number of values assigned to the parameter
   ParameterValuesType values_;  ///< All values assigned to the parameter
 };
 
 std::ostream &operator<<(std::ostream &os, const ParameterValues &parameter_value);
 
 }  // namespace lucid
+
+#ifdef LUCID_INCLUDE_FMT
+
+#include "lucid/util/logging.h"
+
+OSTREAM_FORMATTER(lucid::ParameterValues)
+
+#endif
