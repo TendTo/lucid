ed2004f9b5ff9021c884faf9ad5264195bfd8302
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-06-05 16:49:47 +0200

    chore: use remove in gurobi expressions and made variable naming contingent on NLOG build flag

--- lucid/verification/GurobiLinearOptimiser.cpp
+++ lucid/verification/GurobiLinearOptimiser.cpp
@@ -73,20 +73,23 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
   GRBVar& maxXX = vars[vars.size() - 2];
   GRBVar& minDelta = vars[vars.size() - 1];
 
-  // TODO(tend): we should make variable and constraint naming contingent on a debug flag
+#ifndef NLOG
   c.set(GRB_StringAttr_VarName, "c");
   eta.set(GRB_StringAttr_VarName, "eta");
   minX0.set(GRB_StringAttr_VarName, "minX0");
   maxXU.set(GRB_StringAttr_VarName, "maxXU");
   maxXX.set(GRB_StringAttr_VarName, "maxXX");
   minDelta.set(GRB_StringAttr_VarName, "minDelta");
+#endif
 
   // Variables related to the feature map
   int idx = 0;
   for (GRBVar& var : vars.subspan(0, rkhs_dim)) {
     var.set(GRB_DoubleAttr_LB, -max_num);
     var.set(GRB_DoubleAttr_UB, max_num);
+#ifndef NLOG
     var.set(GRB_StringAttr_VarName, fmt::format("b[{}]", idx++));
+#endif
   }
   c.set(GRB_DoubleAttr_LB, 0);
   c.set(GRB_DoubleAttr_UB, max_num);
@@ -110,7 +113,11 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
   // 1) eta + c*T >= 0 by design
   // 2) eta + c*T <= gamma
   LUCID_DEBUG("Restricting safety probabilities to be positive");
-  model.addConstr(eta + T_ * c, GRB_LESS_EQUAL, gamma_).set(GRB_StringAttr_ConstrName, "eta+c*T<=gamma");
+  model
+      .addConstr(eta + T_ * c, GRB_LESS_EQUAL, gamma_)
+#ifndef NLOG
+      .set(GRB_StringAttr_ConstrName, "eta+c*T<=gamma");
+#endif
 
   // TODO(tend): since we are using row major order, there is no need to copy each row of the matrix.
   //  Just add an assertion to make sure this is not changed at a later date, breaking the LP problem.
@@ -123,10 +130,18 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
     GRBLinExpr expr{};
     expr.addTerms(phi_mat.row(row).eval().data(), vars_.get(), static_cast<int>(phi_mat.cols()));
     expr += maxXX * maxXX_coeff;
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x)>=hatxi[{}]", row));
-    expr -= maxXX * maxXX_coeff;  // TODO(tend): use expr.remove(maxXX);
+    model
+        .addConstr(expr, GRB_GREATER_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x)>=hatxi[{}]", row));
+#endif
+    expr.remove(maxXX);
     expr += -maxXX;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x)<=maxXX[{}]", row));
+    model
+        .addConstr(expr, GRB_LESS_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x)<=maxXX[{}]", row));
+#endif
   }
 
   LUCID_DEBUG_FMT(
@@ -138,10 +153,19 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
     GRBLinExpr expr{};
     expr.addTerms(f0_lattice.row(row).eval().data(), vars_.get(), static_cast<int>(f0_lattice.cols()));
     expr += -fctr1 * eta - fctr2 * minX0;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=hateta[{}]", row));
-    expr -= -fctr1 * eta - fctr2 * minX0;  // TODO(tend): use expr.remove(eta); expr.remove(minX0);
+    model
+        .addConstr(expr, GRB_LESS_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=hateta[{}]", row));
+#endif
+    expr.remove(eta);
+    expr.remove(minX0);
     expr += -minX0;
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=minX0[{}]", row));
+    model
+        .addConstr(expr, GRB_GREATER_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x_0)>=minX0[{}]", row));
+#endif
   }
 
   LUCID_DEBUG_FMT(
@@ -153,29 +177,43 @@ bool GurobiLinearOptimiser::solve(ConstMatrixRef f0_lattice, ConstMatrixRef fu_l
     GRBLinExpr expr{};
     expr.addTerms(fu_lattice.row(row).eval().data(), vars_.get(), static_cast<int>(fu_lattice.cols()));
     expr += -fctr2 * maxXU;
-    model.addConstr(expr, GRB_GREATER_EQUAL, unsafe_rhs)
+    model
+        .addConstr(expr, GRB_GREATER_EQUAL, unsafe_rhs)
+#ifndef NLOG
         .set(GRB_StringAttr_ConstrName, fmt::format("B(x_u)>=hatgamma[{}]", row));
-    expr -= -fctr2 * maxXU;  // TODO(tend): use expr.remove(maxXU);
+#endif
+    expr.remove(maxXU);
     expr += -maxXU;
-    model.addConstr(expr, GRB_LESS_EQUAL, 0).set(GRB_StringAttr_ConstrName, fmt::format("B(x_u)<=maxXU[{}]", row));
+    model
+        .addConstr(expr, GRB_LESS_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(x_u)<=maxXU[{}]", row));
+#endif
   }
 
   LUCID_DEBUG_FMT(
       "Kushner constraints (verification case) - {} constraints\n"
-      "for all x: [ B_plus(x) - B(x) <= hatDelta ] AND [ B(x) >= minDelta ]\n"
+      "for all x: [ B(xp) - B(x) <= hatDelta ] AND [ B(x) >= minDelta ]\n"
       "hatDelta = 2 / (C + 1) * (c - epsilon*Bnorm*kappa_x) + (C - 1) / (C + 1) * minDelta",
       phi_mat.rows() * 2);
   auto mult = w_mat - b_kappa_ * phi_mat;
   for (Index row = 0; row < mult.rows(); ++row) {
     GRBLinExpr expr{};
     expr.addTerms(mult.row(row).eval().data(), vars_.get(), static_cast<int>(mult.cols()));
-    expr += -fctr1 * (c)-fctr2 * minDelta;  // TODO(tend): c − εB̄κ
-    model.addConstr(expr, GRB_LESS_EQUAL, kushner_rhs)
-        .set(GRB_StringAttr_ConstrName, fmt::format("B_plus(x)-B(x)<=hatDelta[{}]", row));
-    expr -= -fctr1 * (c)-fctr2 * minDelta;  // TODO(tend): use expr.remove(c); expr.remove(minDelta);
+    expr += -fctr1 * c - fctr2 * minDelta;  // TODO(tend): c − εB̄κ
+    model
+        .addConstr(expr, GRB_LESS_EQUAL, kushner_rhs)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(xp)-B(x)<=hatDelta[{}]", row));
+#endif
+    expr.remove(c);
+    expr.remove(minDelta);
     expr += -minDelta;
-    model.addConstr(expr, GRB_GREATER_EQUAL, 0)
-        .set(GRB_StringAttr_ConstrName, fmt::format("B_plus(x)-B(x)>=minDelta[{}]", row));
+    model
+        .addConstr(expr, GRB_GREATER_EQUAL, 0)
+#ifndef NLOG
+        .set(GRB_StringAttr_ConstrName, fmt::format("B(xp)-B(x)>=minDelta[{}]", row));
+#endif
   }
 
   // Objective function (η + cT)
