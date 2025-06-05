65187c050c39f56df062f4e0648ec4b596b54692
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-06-05 13:43:32 +0200

    fix: use default constructor for aggregator struct

--- lucid/model/LbfgsTuner.h
+++ lucid/model/LbfgsTuner.h
@@ -19,23 +19,6 @@ namespace lucid {
  * to avoid a transitive dependency on the external library in the header.
  */
 struct LbgsParameters {
-  LbgsParameters() = default;
-  LbgsParameters(const int m_, const Scalar epsilon_, const Scalar epsilon_rel_, const int past_, const Scalar delta_,
-                 const int max_iterations_, const int linesearch_, const int max_submin_, const int max_linesearch_,
-                 const Scalar min_step_, const Scalar max_step_, const Scalar ftol_, const Scalar wolfe_)
-      : m(m_),
-        epsilon(epsilon_),
-        epsilon_rel(epsilon_rel_),
-        past(past_),
-        delta(delta_),
-        max_iterations(max_iterations_),
-        linesearch(linesearch_),
-        max_submin(max_submin_),
-        max_linesearch(max_linesearch_),
-        min_step(min_step_),
-        max_step(max_step_),
-        ftol(ftol_),
-        wolfe(wolfe_) {}
   /**
    * The number of corrections to approximate the inverse Hessian matrix.
    * The L-BFGS routine stores the computation results of previous \ref m
