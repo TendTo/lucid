90a7fdc1d11bf1431fa1cb6c758d0328ed6ce6db
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-07-30 17:13:52 +0200

    feat: add configuration

---
+++ docs/paper/code/configuration.py
@@ -0,0 +1,20 @@
+from pylucid import *
+import numpy as np
+
+def scenario_config() -> Configuration:
+    np.random.seed(42)
+    random.seed(42)
+    c = Configuration()
+    c.seed = 42
+    c.X_bounds = RectSet([(-1, 1)])
+    c.X_init = RectSet([(-0.5, 0.5)])
+    c.X_unsafe = MultiSet(RectSet([(-1, -0.9)]), RectSet([(0.9, 1)]))
+    c.x_samples = np.array([[1.0], [-1.0]])
+    c.xp_samples = np.array([[0.5], [-0.5]])
+    c.lambda_ = 1.0e-5
+    c.time_horizon = 15
+    c.sigma_f = 18.0
+    c.sigma_l = 0.034
+    c.num_frequencies = 5
+    c.oversample_factor = 64.0
+    return c
