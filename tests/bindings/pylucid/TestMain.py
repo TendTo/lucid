46f4bc9daf679c1607373efcb77644d0b9d9e135
Ernesto Casablanca <casablancaernesto@gmail.com>
2026-03-27 12:52:19 +0000

    fix: cuda integration

--- tests/bindings/pylucid/TestMain.py
+++ tests/bindings/pylucid/TestMain.py
@@ -3,7 +3,7 @@ from pylucid import __doc__, __version__
 
 class TestMain:
     def test_version(self):
-        assert __version__ == "0.0.2"
+        assert __version__ == "0.0.3"
 
     def test_docstring(self):
         assert __doc__ == "Lifting-based Uncertain Control Invariant Dynamics"
